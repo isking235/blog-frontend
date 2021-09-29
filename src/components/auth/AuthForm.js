e3c674a8019143f1c5b506480341db51b7745c82
sangho235 <sangho235@it.gsshop.com>
2021-09-30 00:05:26 +0900

    24.2 회원가입과 로그인 구현

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+/**
+ * 회원 가입 도는 로그인 폼을 보여 줍니다.
+ * 
+ */
+
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+    return (
+        <AuthFormBlock>
+            AuthForm
+        </AuthFormBlock>
+    );
+};
+
+export default AuthForm;
