2c06424d3be97503b275a3ea5f3aeda6919b1554
sangho235 <sangho235@it.gsshop.com>
2021-09-29 23:28:01 +0900

    24.1 완료

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>,
+  </Provider>,
   document.getElementById('root')
 );
