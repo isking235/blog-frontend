928f374f880d11b2eeca30900a6b8daa652b566b
sangho235 <sangho235@it.gsshop.com>
2021-10-14 23:19:24 +0900

    오타 수정

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import {all} from 'redux=saga/effects';
+import {all} from 'redux-saga/effects';
 import auth,{authSaga} from './auth';
 import loading from './loading';
 
