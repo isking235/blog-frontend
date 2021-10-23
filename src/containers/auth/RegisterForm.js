f33f71b6efd586b4a0b54ccae7b5a0b89d99f84d
sangho235 <sangho235@it.gsshop.com>
2021-10-23 15:05:44 +0900

    24.2.6  회원 이증 에러 처리하기 까지 완료

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 
@@ -9,6 +9,7 @@ import { withRouter } from 'react-router';
 
 
 const RegisterForm = ({history}) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const {form, auth, authError, user} = useSelector(({auth, user}) => ({
         form: auth.register,
@@ -33,8 +34,18 @@ const RegisterForm = ({history}) => {
      const onSubmit = e => {
          e.preventDefault();
          const {username, password, passwordConfirm} = form;
+
+        //하나라도 비어 있다면
+        if([username, password, password].includes('')) {
+            setError('빈 칸을 모두 입력하세요');
+            return;
+        }
+
+        //비밀번호가 일지 하지 않는다면
          if(password !== passwordConfirm) {
-             //TODO: 오류처리
+             setError('비밀번호가 일치하지 않습니다');
+             dispatch(changeField({ form:'register', key: 'password', value:''}));
+             dispatch(changeField({form: 'register', key: 'passwordConfirm', value:''}));             
              return;
          }
          dispatch(register({username, password}));
@@ -48,8 +59,14 @@ const RegisterForm = ({history}) => {
       //회원 가입 성공/실패 처리
       useEffect(()=> {
           if(authError) {
-              console.log('오류 발생');
-              console.log(authError);
+              //계정명이 이미 존재 할 때
+              if(authError.response.status === 409) {
+                  setError('이미 존재하는 계정명입니다.');
+                  return;
+              }
+
+              //기타 이유
+              console.log('회원가입 실패');
               return;
           }
           if(auth) {
@@ -74,6 +91,7 @@ const RegisterForm = ({history}) => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
           />
       );
 
