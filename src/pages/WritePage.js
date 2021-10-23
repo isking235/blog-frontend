48c9829c7d6a5b7f74b4b0702e54baff04f470c7
sangho235 <sangho235@it.gsshop.com>
2021-10-23 21:38:18 +0900

    25.2.1.1 WriteActionbuttons 만들기

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,12 +1,14 @@
 import Responsive from "../components/common/Responsive";
 import Editor from "../components/write/Editor";
 import TagBox from "../components/write/TagBox";
+import WriteActionButtons from "../components/write/WriteActionbuttons";
 
 const WritePage = () => {
     return (
         <Responsive>
             <Editor />
             <TagBox />
+            <WriteActionButtons/>
         </Responsive>
     )
 }
