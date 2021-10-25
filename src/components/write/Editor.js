4c7d7ac145776ec54dc7567cc13960c4ea394636
sangho235 <sangho235@it.gsshop.com>
2021-10-25 22:39:58 +0900

    25.3 리덕스로 글쓰기 상태 관리하기

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -1,10 +1,9 @@
-import { useEffect } from "react";
-import { useRef } from "react";
-import Quill from "quill";
+import React, { useRef, useEffect} from 'react';
+import Quill from 'quill';
 import 'quill/dist/quill.bubble.css';
-import styled from "styled-components";
-import palette from "../../lib/styles/palette";
-import Responsive from "../common/Responsive";
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
 
 const Editorblock = styled(Responsive)`
     /*페이지 위아래 여백 지정 */
@@ -36,7 +35,7 @@ const QurillWrapper = styled.div`
     }
 `;
 
-const Editor = () => {
+const Editor = ({title, body, onChangeField}) => {
     const quillElement = useRef(null); //Quill을 적용할 DivElement를 설정
     const quillInstance = useRef(null); //Quil 인스턴스를 설정
 
@@ -55,11 +54,28 @@ const Editor = () => {
                 ],
             },
         });
-    },[]);
+
+        //quill에 text-change 이벤트 핸들러등록
+        //참고 : https://quilljs.com/docs/api/#events
+        const quill = quillInstance.current;
+        quill.on('text-change', (delta, oldDelta, source) => {
+            if(source === 'user') {
+                onChangeField({key : 'body', value: quill.root.innerHTML});
+            }
+        });
+    },[onChangeField]);
+
+    const onChangeTitle = e => {
+        onChangeField({key:'title', value: e.target.value});
+    };
 
     return (
         <Editorblock>
-            <TitleInput placeholder="제목을 입력하세요" />
+            <TitleInput 
+                placeholder="제목을 입력하세요" 
+                onChange={onChangeTitle}
+                value= {title}
+            />
             <QurillWrapper>
                 <div ref={quillElement} />
             </QurillWrapper>
