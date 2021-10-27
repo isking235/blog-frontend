fba8a002e860c6532b166a540e1ad2a96244daa1
sangho235 <sangho235@it.gsshop.com>
2021-10-28 00:03:32 +0900

    26.2.4 페이지네이션 구하기 진행중

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -23,15 +23,17 @@ export function* postsSaga() {
 
 const initalState = {
 	posts:null,
-	error:null
+	error:null,
+	lastPage:1,
 	
 };
 
 const posts = handleActions(
 	{
-		[LIST_POSTS_SUCCESS]:(state, {payload:posts}) => ({
+		[LIST_POSTS_SUCCESS]:(state, {payload:posts, meta:response}) => ({
 			...state,
 			posts,
+			lastPage: parseInt(response.headers['last-page'], 10), //문자열을 숫자로 변환
 		}),
 		[LIST_POSTS_FAILURE]:(state, {payload: error}) => ({
 			...state,
